45c2a9158753ce27ba48db25c21c314a3de7d012
Maximilian Keßler <git@maximilian-kessler.de>
2026-07-13 17:32:06 +0200

    set up VersoSlides

---
+++ Slides.lean
@@ -0,0 +1,14 @@
+import VersoSlides
+import Verso.Doc.Concrete
+
+open VersoSlides
+
+#doc (Slides) "Formalising Complexity theory" =>
+
+# First Slide
+
+Content of the first slide.
+
+# Second Slide
+
+Content of the second slide.
